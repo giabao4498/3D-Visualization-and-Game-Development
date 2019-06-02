f851b1a7e6e4e574307d1b8c86100d07c8a92bda
giabao4498 <giabao441998@gmail.com>
2019-06-02 21:57:27 +0700

    Upload A02

---
+++ A02/Assets/Scripts/CreateBoard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBoard : MonoBehaviour
+{
+    public static CreateBoard board;
+    public List<Sprite> club = new List<Sprite>();
+    public GameObject prefab;
+    public int M, N;
+    void Start()
+    {
+        board = GetComponent<CreateBoard>();
+        Vector2 child = prefab.GetComponent<SpriteRenderer>().bounds.size;
+        GameObject[,] matrix = new GameObject[M, N];
+        float startX = -8;
+        float startY = -4;
+        GameObject tile;
+        Sprite sprite;
+        for(int i = 0; i < M; ++i)
+        for(int j = 0; j < N; ++j)
+        {
+            tile = Instantiate(prefab, new Vector3(startX + child.x * i, startY + child.y * j, 0), prefab.transform.rotation);
+            matrix[i,j] = tile;
+            tile.transform.parent = transform;
+            sprite = club[Random.Range(0, club.Count)];
+            tile.GetComponent<SpriteRenderer>().sprite = sprite;
+        }
+    }
+}
