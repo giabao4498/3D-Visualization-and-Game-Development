47f7c5bab43d3eac5f7da3b73aa740feb9c3413d
giabao4498 <giabao441998@gmail.com>
2019-06-12 22:16:50 +0700

    Upload A03

---
+++ A03/Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private Transform target;
+    [SerializeField]
+    private float xMax;
+    [SerializeField]
+    private float yMax;
+    [SerializeField]
+    private float xMin;
+    [SerializeField]
+    private float yMin;
+    void Start()
+    {
+        target = GameObject.Find("Zero_14").transform;
+    }
+    void LateUpdate()
+    {
+        if (target)
+            transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+    }
+}
---
+++ A03/Assets/Scripts/Zero.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class Zero : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private Animator animator;
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+    [SerializeField]
+    private Transform[] groundPoints;
+    [SerializeField]
+    private float radius;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private bool airControl;
+    [SerializeField]
+    private float jumpHeight;
+    private bool isRight;
+    private bool isGrounded;
+    private bool isJumped;
+    const int airLayer = 1;
+    private string[] enemy = new string[] {
+        "Sigma_0",
+        "Enemy1_5",
+        "Enemy2_0"
+    };
+    void Start()
+    {
+        isRight = true;
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (gameObject)
+            Inp();
+    }
+    void FixedUpdate()
+    {
+        if (gameObject)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            isGrounded = IsOnGround();
+            Move(horizontal);
+            Flip(horizontal);
+            Layer();
+            Reset();
+        }
+    }
+    private void Inp()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            isJumped = true;
+    }
+    private void Reset()
+    {
+        isJumped = false;
+    }
+    private void Move(float horizontal)
+    {
+        if (rigidbody.velocity.y < 0)
+            animator.SetBool("land", true);
+        if (isGrounded && isJumped)
+        {
+            isGrounded = false;
+            rigidbody.AddForce(new Vector2(0, jumpHeight));
+            animator.SetTrigger("jump");
+        }
+        rigidbody.AddForce(new Vector2(horizontal * moveSpeed, 0));
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+    }
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !isRight || horizontal < 0 && isRight)
+        {
+            isRight = !isRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+    private bool IsOnGround()
+    {
+        if (rigidbody.velocity.y <= 0)
+        foreach (Transform point in groundPoints)
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, radius, whatIsGround);
+            for (int i = 0; i < colliders.Length; i++)
+            if (colliders[i].gameObject != gameObject)
+            {
+                animator.ResetTrigger("jump");
+                animator.SetBool("land", false);
+                return true;
+            }
+        }
+        return false;
+    }
+    private void Layer()
+    {
+        if (!isGrounded)
+            animator.SetLayerWeight(airLayer, 1);
+        else
+            animator.SetLayerWeight(airLayer, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (enemy.Contains(collision.gameObject.name))
+            Destroy(gameObject);
+    }
+}
